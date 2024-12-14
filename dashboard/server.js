48aafc4a001caa0b7199047cb19ec1d888dd5211
Ryan Donohue <taskcomm@gmail.com>
2024-12-14 15:20:03 +1000

    v0.3

--- dashboard/server.js
+++ dashboard/server.js
@@ -4,7 +4,6 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const app = express();
 const PORT = 3000;
 
